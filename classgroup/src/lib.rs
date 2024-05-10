7ea644a5930c79edb5184fe97e0394d1600173a3
Julien Coolen <jtcoolen@pm.me>
2024-05-10 16:38:55 +0200

    remove unused modules

--- classgroup/src/lib.rs
+++ classgroup/src/lib.rs
@@ -7,7 +7,7 @@ pub mod context;
 pub mod hash_to_point;
 pub mod random_oracle;
 
-pub mod bls_signature;
+//pub mod bls_signature;
 pub mod key_pop_zk;
 
 pub mod bls12381_serde;
@@ -23,10 +23,10 @@ pub mod cg_encryption;
 pub mod nidkg_dealing;
 pub mod nidkg_zk_share;
 pub mod nizk_dleq;
-pub mod threshold_signature;
+//pub mod threshold_signature;
 pub mod utils;
 pub mod constants;
 
-#[cfg(test)]
-pub mod test_utils;
+//#[cfg(test)]
+//pub mod test_utils;
 
